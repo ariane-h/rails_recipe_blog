23639b5da355e40c51e70ffa76b3eb2664f78e83
Ariane Holford <aribomop@gmail.com>
2020-08-11 16:06:05 +0100

    add comment functionality to blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_124955) do
+ActiveRecord::Schema.define(version: 2020_08_11_143351) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
